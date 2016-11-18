054d8661ac4024c238f54497fe8f4aadcd93eb9a
sainath-batthala <sainath.b@practo.com>
2016-11-18 15:05:07 +0530

    bash script dailyquote.sh added.

---
+++ dailyquote.sh
@@ -0,0 +1,7 @@
+# get quote by sending request to api
+quoteoutput=`curl -sb -H "Accept: application/json" "http://quotes.rest/qod.json?category=inspire" | grep '"quote":'`
+quoteoutput=${quoteoutput#*:}
+quoteoutput=${quoteoutput::${#quoteoutput}-1}
+# output the quote
+echo "Daily Quote"
+echo $quoteoutput
