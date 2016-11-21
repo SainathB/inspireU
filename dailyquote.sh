b0082218bcd61f9d3f9829fc2bc8de8104b9a0f9
sainath-batthala <sainath.b@practo.com>
2016-11-21 09:58:07 +0530

    Service Connectivity check added.

--- dailyquote.sh
+++ dailyquote.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
-# get quote by sending request to api
-quoteoutput=`curl -sb -H "Accept: application/json" "http://quotes.rest/qod.json?category=inspire" | grep '"quote":'`
-quoteoutput=${quoteoutput#*:}
-quoteoutput=${quoteoutput::${#quoteoutput}-1}
-# output the quote
-echo "Daily Quote"
-echo $quoteoutput
+# check if url is reachable
+ping -c 1 quotes.rest &> /dev/null
+if [ $? -eq 0 ]; then
+	# get quote by sending request to api
+	quoteoutput=`curl -sb -H "Accept: application/json" "http://quotes.rest/qod.json?category=inspire" | grep '"quote":'`
+	quoteoutput=${quoteoutput#*:}
+	quoteoutput=${quoteoutput::${#quoteoutput}-1}
+	#output the quote
+	echo "Daily Quote"
+	echo $quoteoutput
+fi
