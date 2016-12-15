f8bd6d0115e7bd09fff67589dee6697eea7e274d
sainath-batthala <sainath.b@practo.com>
2016-12-15 17:09:47 +0530

    Added cache for Daily Quote

---
+++ cachequote.sh
@@ -0,0 +1,2 @@
+DATE=2016-12-15
+QUOTE="Be daring, be different, be impractical, be anything that will assert integrity of purpose and imaginative vision against the playitsafers, the creatures of the commonplace, the slaves of the ordinary."
--- dailyquote.sh
+++ dailyquote.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+# load cachequote
+source `dirname $0`/cachequote.sh
+if [ ! -z $DATE ] && [ $DATE == `date +%Y-%m-%d` ] && [ ! -z "$QUOTE" ]; then
+		echo "Daily Quote"
+		echo $QUOTE
+		exit 0
+fi
 # check if url is reachable
 ping -c 1 quotes.rest &> /dev/null
 if [ $? -eq 0 ]; then
@@ -9,4 +16,9 @@ if [ $? -eq 0 ]; then
 	#output the quote
 	echo "Daily Quote"
 	echo $quoteoutput
+	rm cachequote.sh
+	touch cachequote.sh
+	chmod +x cachequote.sh
+	echo "DATE=`date +%Y-%m-%d`" >> cachequote.sh
+	echo QUOTE=`echo $quoteoutput` >> cachequote.sh
 fi
